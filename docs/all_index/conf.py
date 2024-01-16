2dff074e3c322c97cfb6987c40d20a557413675a
IvanVP1 <145982366+IvanVP1@users.noreply.github.com>
2024-01-16 14:13:32 +0400

    Update conf.py

--- docs/all_index/conf.py
+++ docs/all_index/conf.py
@@ -18,6 +18,7 @@
 #
 
 import os, sys, datetime
+import sphinx_autodoc_typehints
 import sphinx_rtd_theme
 
 # sys.path.insert(0, os.path.abspath('.'))
@@ -33,7 +34,18 @@ now = datetime.datetime.now()
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo']
+extensions = [
+    'sphinx_autodoc_typehints',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
